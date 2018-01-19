2a6e8cef6ac5f984a9e0709defe2071669b79b0c
Justin Wickenheiser <wickenju@gvsu.edu>
2018-01-19 16:21:42 -0500

    worked on chart class prototype definition

--- C++/chart.h
+++ C++/chart.h
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <string>
 #include "coord.h"
 #include "performer.h"
 
@@ -6,8 +7,19 @@
 #define CHART_H
 	class chart {
 		private:
-			
+			int chartId;
+			string title;
 		public:
+			// constructors
+			chart();
+			chart(string);
 
+			// public methods
+			void setTitle(string);
+			string getTitle();
+
+			// Overloaded operators
+			friend std::ostream &operator<<(std::ostream &os, const chart& c);
+			void operator = (const chart& c);
 	};
 #endif
